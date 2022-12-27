f649cc940dc29c0d2b045fc1fa8f488041170510
Sam <saml@lyke.co.uk>
2022-12-27 14:27:56 +0000

    Added Bootstrap

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <button className="btn btn-primary">btn</button>
         <a
           className="App-link"
           href="https://reactjs.org"
